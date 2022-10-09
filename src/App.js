6a8a23c13e127ac061e7ae5aaf27f5a61718df89
yousefkmail <yousef.hnaty@gmail.com>
2022-10-09 20:16:28 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route
+            element={
+              <div>
+                NAVBAR
+                <Outlet />
+              </div>
+            }
+          >
+            <Route path="home" element={<div>Me in home </div>} />
+            <Route path="about" element={<div>Me in about </div>} />
+            <Route path="contact" element={<div>Me in contact </div>} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
