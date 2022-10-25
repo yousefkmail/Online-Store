490a438acf4b6c077cb8b1295d8b7a9794f3dd6a
yousefkmail <yousef.hnaty@gmail.com>
2022-10-25 15:37:05 +0300

    Contact page branch created

---
+++ src/Components/ContactInfo.tsx
@@ -0,0 +1,17 @@
+import style from "../Styles/Contact.module.css";
+
+interface contact {
+  info: string;
+  value: string;
+}
+
+const Contactinfo = (props: contact) => {
+  return (
+    <div className={style.contactinfo}>
+      <h4>{props.info} :</h4>
+      <p>{props.value}</p>
+    </div>
+  );
+};
+
+export default Contactinfo;
