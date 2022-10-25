df1b63848e5a4fb8757bd7079c156f624a518462
yousefkmail <yousef.hnaty@gmail.com>
2022-10-25 15:17:49 +0300

    Strings refactored

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,32 +1,20 @@
 import style from "../Styles/About.module.css";
 import Services from "./Services";
 import Personprofile from "./PersonProfile";
+import { ABOUT_US_INFO, ABOUT_US_LABEL, ABOUT_US_TITLE } from "../constants";
 const About = () => {
   return (
     <div className={style.container}>
       <div className={style.header}>
         <div className={style.headercontent}>
-          <h1>About us</h1>
-          <h2>We deliver fast services </h2>
+          <h1>{ABOUT_US_LABEL}</h1>
+          <h2>{ABOUT_US_TITLE} </h2>
         </div>
       </div>
 
       <div className={style.personalinfo}>
         <div className={style.personaldescription}>
-          <p>
-            As a result, Business IT Essentials has been around for 20+ years
-            now, has worked with hundreds of clients across 4 continents in
-            hundreds of industries. Our clients come to us and stay with us
-            because they know how important it is to have someone they can trust
-            with taking care of their personal or business website. Our clients
-            seek our services because they want the peace of mind that their
-            websites are in good hands. Our clients work with us because we save
-            them time and money by providing complete care for their websites
-            for a low fixed monthly price and an unbeatable 100% satisfaction
-            guarantee. Our clients love us for the personal attention they
-            receive, our quick responses, problem resolution skills, and of
-            course our affordable services.
-          </p>
+          <p> {ABOUT_US_INFO} </p>
         </div>
         <div className={style.personalinfo}>
           <Personprofile />
--- src/constants.tsx
+++ src/constants.tsx
@@ -1,29 +1,48 @@
-export const   LINK_About = "About"; 
-export const   LINK_Contact = "Contact"; 
-export const   LINK_Home = "Home";
-export const   ADDRESS = "her business/home address"
-export const  PHONE_NUMBER="056943555"
-export const  EMAIL="yousef.hnaty@gmail.com"
-export const  WEBSITE="www.cuebat.com"
-export const  LINK_ABOUT = "about"
-export const  LINK_CONTACT = "contact"
-export const  LINK_HOME = "home"
-export const  FAVORITE_ICON= "fa-regular fa-star fa-2xl"
-export const  ADDTOCART_ICON =" fa-solid fa-cart-plus fa-2xl"
-export const  INCART_ICON = "fa-solid fa-cart-shopping fa-2xl"
-export const  INFAVORITE_ICON = "fa-solid fa-star fa-2xl"
-export const  WEBSITE_LOGO =" http://cardwellchiropractic.com/static/media/cardwell-logo1.fb8cf282.png"
-export const  PRICE_SLIDER_LABEL = "Pick Your Price  "
-export const  CONTACTUS_NAME = "Name"
-export const  CONTACTUS_EMAIL = "Email"
-export const  CONTACTUS_SUBJECT = "Subject"
-export const  CONTACTUS_MESSAGE = "Message"
-export const  CONTACTUS_NUMBER_LABEL = "Number"
-export const  CONTACTUS_ADDRESS_LABEL = "Address"
-export const  CONTACTUS_EMAIL_LABEL = "Email"
-export const  CONTACTUS_WEBSITE_LABEL = "Website"
-export const ITEM_IMAGE = "https://media.istockphoto.com/vectors/blank-white-screen-laptop-isolated-vector-id1266659893?k=20&m=1266659893&s=612x612&w=0&h=TQ5qqyApqEQ8XlWKhfvuYUR5_Hcfi2P_h3ou6o5Us6g="
-export const IMAGE_NOT_FOUND = "Cannot preview image"
-export const CONTACT_US_MESSAGE = "Contact us "
-export const SENDING_MESSAGE_HEADER = "Send us a message"
-export const TOGGLEBUTTON = "Toggle button"
+export const LINK_About = "About";
+export const LINK_Contact = "Contact";
+export const LINK_Home = "Home";
+export const ADDRESS = "her business/home address";
+export const PHONE_NUMBER = "056943555";
+export const EMAIL = "yousef.hnaty@gmail.com";
+export const WEBSITE = "www.cuebat.com";
+export const LINK_ABOUT = "about";
+export const LINK_CONTACT = "contact";
+export const LINK_HOME = "home";
+export const FAVORITE_ICON = "fa-regular fa-star";
+export const ADDTOCART_ICON = " fa-solid fa-cart-plus";
+export const INCART_ICON = "fa-solid fa-cart-shopping ";
+export const INFAVORITE_ICON = "fa-solid fa-star";
+export const WEBSITE_LOGO =
+  " http://cardwellchiropractic.com/static/media/cardwell-logo1.fb8cf282.png";
+export const PRICE_SLIDER_LABEL = "Pick Your Price  ";
+export const CONTACTUS_NAME = "Name";
+export const CONTACTUS_EMAIL = "Email";
+export const CONTACTUS_SUBJECT = "Subject";
+export const CONTACTUS_MESSAGE = "Message";
+export const CONTACTUS_NUMBER_LABEL = "Number";
+export const CONTACTUS_ADDRESS_LABEL = "Address";
+export const CONTACTUS_EMAIL_LABEL = "Email";
+export const CONTACTUS_WEBSITE_LABEL = "Website";
+export const ITEM_IMAGE =
+  "https://media.istockphoto.com/vectors/blank-white-screen-laptop-isolated-vector-id1266659893?k=20&m=1266659893&s=612x612&w=0&h=TQ5qqyApqEQ8XlWKhfvuYUR5_Hcfi2P_h3ou6o5Us6g=";
+export const IMAGE_NOT_FOUND = "Cannot preview image";
+export const CONTACT_US_MESSAGE = "Contact us ";
+export const SENDING_MESSAGE_HEADER = "Send us a message";
+export const TOGGLEBUTTON = "Toggle button";
+export const COLLECTION_DATA_NAME = "Items";
+export const COLLECTION_SERVICES_NAME = "Services";
+
+export const firebaseConfig_const = {
+  apiKey: "AIzaSyDBf4F9P_jkRCNTaeSIbUxZoS9G_YDh_SE",
+  authDomain: "online-store-dev-27c54.firebaseapp.com",
+  projectId: "online-store-dev-27c54",
+  storageBucket: "online-store-dev-27c54.appspot.com",
+  messagingSenderId: "687343091390",
+  appId: "1:687343091390:web:a3a14a1bb8e239550ce8ef",
+};
+
+export const ABOUT_US_INFO =
+  " As a result, Business IT Essentials has been around for 20+ yearsnow, has worked with hundreds of clients across 4 continents inhundreds of industries. Our clients come to us and stay with usbecause they know how important it is to have someone they can trust seek our services because they want the peace of mind that their websites are in good hands. Our clients work with us because we save them time and money by providing complete care for their websites for a low fixed monthly price and an unbeatable 100% satisfaction guarantee. Our clients love us for the personal attention they receive, our quick responses, problem resolution skills, and ofcourse our affordable services";
+
+export const ABOUT_US_LABEL = "About us ";
+export const ABOUT_US_TITLE = "We deliver fast services";
