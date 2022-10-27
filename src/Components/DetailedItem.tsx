c333b4eb41e24f1234b2710cbe5d9619032879f0
yousefkmail <yousef.hnaty@gmail.com>
2022-10-27 09:44:17 +0300

    FIX : Refactor strings of detailed items page

--- src/Components/DetailedItem.tsx
+++ src/Components/DetailedItem.tsx
@@ -6,6 +6,16 @@ import style from "../Styles/Detaileditem.module.css";
 import Shoppingitem from "./ShoppingItem";
 import { useNavigate } from "react-router-dom";
 import Shoppingitems from "./Shoppingitems";
+
+import {
+  ADD_TO_CART,
+  ADD_TO_FAVORITE,
+  IMAGE_NOT_FOUND,
+  RELATED_ITEMS,
+  REMOVE_FROM_CART,
+  REMOVE_FROM_FAVORITE,
+  EXCEPTIONS_ARRAY,
+} from "../constants";
 const Detaileditem = () => {
   const { id } = useParams();
   const navigate = useNavigate();
@@ -27,7 +37,7 @@ const Detaileditem = () => {
   function getitemdata() {
     const array: any = [];
     Object.entries(item).map((item) => {
-      if (item[0] !== "url" && item[0] !== "id")
+      if (!EXCEPTIONS_ARRAY.includes(item[0]))
         array.push(
           <div key={item[0]} className={style.infodetail}>
             {item[0] + " : " + item[1]}
@@ -39,23 +49,20 @@ const Detaileditem = () => {
   return (
     <div>
       <div className={style.detaileditem}>
-        <img src={item.url} alt="" />
+        <img src={item.url} alt={IMAGE_NOT_FOUND} />
         <div className={style.infowrapper}>
           <button onClick={() => Addtofavorite(item)}>
-            {!Favorite.includes(item)
-              ? "   Add to Favorite"
-              : "Remove from favorite "}
+            {!Favorite.includes(item) ? ADD_TO_FAVORITE : REMOVE_FROM_FAVORITE}
           </button>
           <button onClick={() => Addtocart(item)}>
-            {" "}
-            {!Cart.includes(item) ? "   Add to Cart" : "Remove from Cart "}
+            {!Cart.includes(item) ? ADD_TO_CART : REMOVE_FROM_CART}
           </button>
 
           {getitemdata()}
         </div>
       </div>
       <div>
-        <h2>Related items </h2>
+        <h2>{RELATED_ITEMS}</h2>
       </div>
       <Shoppingitems
         items={Items.filter(
